732467dd4847f57ebcc1c68a638c93b94ae9d8da
Alex Hurduc <kgbbi>
2017-12-09 19:32:36 +0100

    Added Match History in Summoner and new Connection

---
+++ LoLQueen/LoLQueen/Deserialize.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace LoLQueen
+{
+    public class Deserialize
+    {
+        /*Variable used to make the program more universal
+         * Changing the variables allows the program to show
+         * Different users without altering the URL manually
+         */
+
+        private const string ApiKey = "RGAPI-70e6c8fd-701d-43de-8fa4-3d3ac4efd6df";
+
+        private const string SummonerName = "kgbbí";
+
+        private const string Region = "euw1";
+
+        //replace deserialize.cs asyn method with this
+
+        public async static Task<Summoner> ConnectToRiot(string user, string regionName)
+        {
+            var http = new HttpClient();
+            string riotUrl = String.Format(
+                "https://" + Region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + SummonerName + "?api_key=" + ApiKey,
+                regionName, user, ApiKey);
+
+            /* Wait for the url to respond
+             * Wait for the url to return something
+             * Convert and deserialize the json to 
+             * display the information
+             */
+
+        var response = await http.GetAsync(riotUrl);
+
+            //Wait for the url to return something
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<Summoner>(result);
+
+        }
+
+        public async static Task<List<MatchHistory>> ConnectToRito(string user, string regionName)
+        {
+            var http = new HttpClient();
+            string riotUrl = String.Format(
+                "https://" + Region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + SummonerName + "?api_key=" + ApiKey,
+                regionName, user, ApiKey);
+
+            /* Wait for the url to respond
+             * Wait for the url to return something
+             * Convert and deserialize the json to 
+             * display the information
+             */
+
+            var response = await http.GetAsync(riotUrl);
+
+            //Wait for the url to return something
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<List<MatchHistory>>(result);
+
+        }
+    }
+}
+ 
---
+++ LoLQueen/LoLQueen/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace LoLQueen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Entered button click");
+            var obj = await Deserialize.ConnectToRiot("MLPQuadzilla", "EUW1");
+            string output = JsonConvert.SerializeObject(obj);
+            var root = JsonConvert.DeserializeObject<Summoner>(output);
+
+            Label1.Content = root.Name;
+            Label2.Content = root.SummonerLevel;
+        }
+
+        private async Task DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var obj = await Deserialize.ConnectToRito("Harmelody", "EUW1");
+            string output = JsonConvert.SerializeObject(obj);
+            var root = JsonConvert.DeserializeObject<List<MatchHistory>>(output);
+           // dataGridView.DataGrid = output;
+        }
+    }
+}

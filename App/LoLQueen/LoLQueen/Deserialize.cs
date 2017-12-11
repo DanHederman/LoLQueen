6ef1ed3fadd100019970f649fef9e4b004f11312
TomMcNulty97 <c15480708@mydit.ie>
2017-12-11 22:23:28 +0100

    removed test code

--- App/LoLQueen/LoLQueen/Deserialize.cs
+++ App/LoLQueen/LoLQueen/Deserialize.cs
@@ -7,7 +7,6 @@ using Newtonsoft.Json;
 
 namespace LoLQueen
 {
-    //REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
     public class Deserialize
     {
         /*Variable used to make the program more universal
@@ -21,24 +20,16 @@ namespace LoLQueen
 
         private const string Region = "euw1";
 
-        //replace deserialize.cs asyn method with this
 
-        public async static Task<Summoner> ConnectToRiot(string user, string regionName)
+        public static async Task<Summoner> ConnectToRiot(string user, string regionName)
         {
             var http = new HttpClient();
+
             string riotUrl = String.Format(
                 "https://" + Region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + SummonerName + "?api_key=" + ApiKey,
                 regionName, user, ApiKey);
 
-            /* Wait for the url to respond
-             * Wait for the url to return something
-             * Convert and deserialize the json to 
-             * display the information
-             */
-
-        var response = await http.GetAsync(riotUrl);
-
-            //Wait for the url to return something
+            var response = await http.GetAsync(riotUrl);
 
             var result = await response.Content.ReadAsStringAsync();
 
@@ -46,34 +37,21 @@ namespace LoLQueen
 
         }
 
-        public async static Task<MatchList> ObtainMatchList(string accID, string regionName)
+        public static async Task<MatchList> ObtainMatchList(string accID, string regionName)
         {
-            Console.WriteLine("Entered obtaining match hist");
-            var http = new HttpClient();
-            //string riotUrl = String.Format(
-                //"https://" + Region + ".api.riotgames.com/lol/match/v3/matchlists/by-account/"+accID+"/recent?api_key=" + ApiKey,
-                //regionName, ApiKey);
 
-            //"https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + ACCOUNT_ID + "/recent?api_key=" + API_KEY;
+            var http = new HttpClient();
 
             string riotUrl = String.Format("https://{0}.api.riotgames.com/lol/match/v3/matchlists/by-account/{1}/recent?api_key={2}", regionName, accID, ApiKey);
 
-            /* Wait for the url to respond
-             * Wait for the url to return something
-             * Convert and deserialize the json to 
-             * display the information
-             */
 
             var response = await http.GetAsync(riotUrl);
 
-            //Wait for the url to return something
 
             var result = await response.Content.ReadAsStringAsync();
 
-            MatchList matchList = JsonConvert.DeserializeObject<MatchList>(result); // can you place breakpoint here?
+            MatchList matchList = JsonConvert.DeserializeObject<MatchList>(result); 
 
-            
-            //return JsonConvert.DeserializeObject<List<MatchList>>(result); //error
             return matchList;
 
         }
--- App/LoLQueen/LoLQueen/Summoner.cs
+++ App/LoLQueen/LoLQueen/Summoner.cs
@@ -6,15 +6,12 @@ namespace LoLQueen
 
     public class Summoner
     {
-        //internal Summoner summoner;
-        //testing
         public int ProfileIconId { get; set; }
         public string Name { get; set; }
         public int SummonerLevel { get; set; }
         public long RevisionDate { get; set; }
         public long Id { get; set; }
         public long AccountId { get; set; }
-        //DO YOU WANNA REEEEE WITH MEEEEEE
     }
 
 }

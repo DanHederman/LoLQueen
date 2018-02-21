fa04e7e685a14bbe1cf65c5b8f2a8967d0f6127f
Dan Hederman <C15410232@mydit.ie>
2018-02-21 22:22:33 +0100

    Key Removed

--- App/LoLQueen/LoLQueen/RiotUrl.cs
+++ App/LoLQueen/LoLQueen/RiotUrl.cs
@@ -11,7 +11,7 @@
     public class RiotUrl
     {
         
-        static string apiKey = "RGAPI-2ec4087b-52d8-4450-b6c2-ba35dbee6e75";
+        static string apiKey = "";
         /// <summary>
         /// Summoner information
         /// </summary>
